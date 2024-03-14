261b321f402035ce3ba4d54a59efa81ae6c02b4b
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-03-14 14:34:51 +0100

    end milestone 3

--- js/script.js
+++ js/script.js
@@ -7,24 +7,42 @@ const { createApp } = Vue;
 createApp({
   data () {
     return {
+
       contacts,
       counterChat: 0,
-      
+      newMessage: '',
       
     }
 
   },
   methods: {
     
-    findLastMessage(index) {
-      const {messages} = this.contacts[index];
-      
-      return messages[messages.length - 1].message;
-      
-    },
+        findLastMessage(index) {
 
-  },
+          const {messages} = this.contacts[index];
+          
+          return messages[messages.length - 1].message;
+          
+        },
+        sendMessage(index){
+          
+            this.contacts[index].messages.push({
+            date: '10/01/2020 15:50:00',
+            message: this.newMessage,
+            status: 'sent'
+            });
+            setTimeout(() => {
+              this.contacts[index].messages.push({
+                date: '10/01/2020 16:15:22',
+                message: 'Tutto fatto!',
+                status: 'received'
+              });
+            }, 1000);
+
+        }
+
+      },
 
 
-}).mount('#app');  
+    }).mount('#app');  
 
