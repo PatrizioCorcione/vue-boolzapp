9f7ec1b33a2aba36248c03bd5f0876be45fd7523
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-03-14 12:58:47 +0100

    fine prima mileston

--- js/script.js
+++ js/script.js
@@ -2,5 +2,18 @@ import { contacts } from "./data.js";
 
 // Milestone 1
 
+const { createApp } = Vue;
 
+createApp({
+  data () {
+    return {
+      contacts,
+      
+      
+    }
+
+  },
+
+
+}).mount('#app');  
 
