71cc010c7b4d08fe69638decc9b81d7e4a5c3afc
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-03-16 16:32:13 +0100

    [fix]

--- js/script.js
+++ js/script.js
@@ -1,48 +1,43 @@
-import { contacts } from "./data.js";
-
-
-
 const { createApp } = Vue;
 const {DateTime} = luxon;
+import { contacts } from "./data.js";
 
 createApp({
-  data () {
-    return {
 
+  data () {
+
+    return {
       contacts,
       counterChat: 0,
       newMessage: '',
       searchUtente: '',
-      
-      
-      
     }
 
   },
+
   methods: {
-    
+
+    /*dove message e uguale a message nei data*/
     findLastMessage(index) {
       const {messages} = this.contacts[index];
       if (messages.length > 0) {
-
         return messages[messages.length - 1].message;
-        
       }else{
         return 'Nessun messaggio';
       }
       
     },
 
-    sendMessage(index){
-        this.contacts[index].messages.push({
-        date: this.getData(index), 
+    sendMessage(){
+        this.contacts[this.counterChat].messages.push({
+        date: this.getMinut(), 
         message: this.newMessage,
         status: 'sent',
         });
         this.newMessage = '';
         setTimeout(() => {
-          this.accessoUtente = this.getData(index);
-          this.contacts[index].messages.push({
+          this.accessoUtente = this.getMinut(this.counterChat);
+          this.contacts[this.counterChat].messages.push({
             date: this.accessoUtente,
             message: 'Tutto fatto!',
             status: 'received',
@@ -50,29 +45,30 @@ createApp({
         }, 1000);
     },
 
-    seeOption( index){
+    seeOption(index){
       const allMess = document.querySelectorAll('.func-mess');
-      
       allMess[index].classList.toggle('d-none');
-    
+    },
+
+    getMinutMsg(index) {
+      const contact = this.contacts[index];
+      if (contact && contact.messages.length > 0) {
+        const lastMinDate = contact.messages[contact.messages.length - 1].date;
+        return lastMinDate;
+      } else {
+        return 'Nessun messaggio';
+      }
     },
 
     getMinut(){
-    const date = DateTime.now().setLocale('it');
-    return date.toFormat('hh:mm');
-    },
-
-    getData(){
       const date = DateTime.now().setLocale('it');
-      return date.toFormat('dd/MM/yyyy HH:mm');
+      return date.toFormat('dd/MM/yyyy HH:mm:ss');
     },
-
-    
 
   },
 
-  
   computed: {
+
     notAfilter() {
       this.contacts.forEach(contact => {
         if (contact.name.toLowerCase().includes(this.searchUtente.toLowerCase())) {
@@ -80,23 +76,20 @@ createApp({
         } else {
           contact.visible = false;
         }
-        
       });
       return this.contacts;
     },
 
     lastDate(){
       const {messages} = this.contacts[this.counterChat];
-      return messages[messages.length - 1].date;
+      if (messages.length > 0) {
+        return messages[messages.length - 1].date;
+      }else{
+        return 'Nessun messaggio';
+      }
+    },
 
-    }
-
-    
-    
-  },
-
+  }
   
-   
-
 }).mount('#app');  
 
